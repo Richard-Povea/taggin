44867d78a3360fed4777a7369802bfcbe474b6aa
Richard-povea <richardpovea@gmail.com>
2022-10-04 10:55:22 -0300

    Output Directory
    Se crea directorio para cada taxonomía y una función que los exporta ahí.

--- dsp/main.py
+++ dsp/main.py
@@ -4,7 +4,7 @@ import json
 import soundfile as sf
 
 from typing import Optional
-from pydantic import BaseModel, validator, ValidationError
+from pydantic import BaseModel
 
 DATA_FILE_PATH = 'mp_ev_v3.json'
 
@@ -21,7 +21,7 @@ def importData() -> Event:
         events = [Event(**e) for e in data]
     return events
 
-def cutAudioFile(path: str, start: float|int, end: float|int, name: str, output_path: Optional[str]=None) -> None:
+def cutAudioFile(path: str, start: float|int, end: float|int):
     """Cut an audio file\n
         PARAMETERS:
             path: relative path\n
@@ -32,19 +32,23 @@ def cutAudioFile(path: str, start: float|int, end: float|int, name: str, output_
     """
     x, sr = librosa.load(path, sr=None)
     y = x[start*sr : end*sr]
-    if output_path:
-        name = os.path.join(output_path, name)
-    sf.write(name, y, sr)
+    return y, sr
+
+def writeAudioFile(data, sr, file_name: str, taxonomy:str):
+    base_path = 'taggin\media'
+    directory = os.path.join(base_path, taxonomy)
+    path = os.path.join(directory, file_name)
+    sf.write(path, data, sr)
 
 def loopThroughTheData(directory: Optional[str]=None) -> None:
     events = importData()
     if directory:
         path = os.path.join(directory, event.file_name)
     for i, event in enumerate(events):
-        cutAudioFile(path, 
-                     event.start,
-                     event.end,
-                     f'{event.file_name}_{i+1}')
+        y, sr = cutAudioFile(path, 
+                            event.start,
+                            event.end)
+        writeAudioFile(y, sr, f'{event.file_name}_{i+1}', event.taxonomy)
 
 def main() -> None:
     """Main function"""
